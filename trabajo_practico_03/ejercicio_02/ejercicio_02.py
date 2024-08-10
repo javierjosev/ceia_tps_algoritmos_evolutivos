fdbfbdc8796095de7bf2f01f51932c4950d239fb
gmvtrmaga <margomez@gmv.com>
2024-08-10 19:21:35 +0200

    fixed errors on restricted PSO algorithm

--- trabajo_practico_03/ejercicio_02/ejercicio_02.py
+++ trabajo_practico_03/ejercicio_02/ejercicio_02.py
@@ -10,8 +10,8 @@ g2) 20*a + 10*b < 4270
 
 """
 
-from restricted_pso import RestrictedPSO
 import matplotlib.pyplot as plt
+from restricted_pso import RestrictedPSO
 
 
 def plot_vector(vector, num_part):
@@ -19,10 +19,12 @@ def plot_vector(vector, num_part):
 
     if vector is not None:
         x_vector = list(range(len(vector)))
-        ax.plot(x_vector, vector, marker='o', linestyle='-', color='b', label='Iteración')
-        ax.set_xlabel('Iteración')
-        ax.set_ylabel('Valor de GBest')
-        ax.set_title(f'Gráfica de GBest para {num_part} partículas')
+        ax.plot(
+            x_vector, vector, marker="o", linestyle="-", color="b", label="Iteración"
+        )
+        ax.set_xlabel("Iteración")
+        ax.set_ylabel("Valor de GBest")
+        ax.set_title(f"Gráfica de GBest para {num_part} partículas")
         ax.grid(True)
         ax.legend()
 
@@ -41,23 +43,37 @@ max_iterations = 80  # numero máximo de iteraciones para la optimizacion
 c1 = c2 = 2  # coeficientes de aceleracion
 w = 0.5  # factor de inercia
 
-variables = ['a', 'b']
-inequalities = ['300*a + 400*b <= 127000', '20*a + 10*b <= 4270']
+variables = ["a", "b"]
+inequalities = ["300*a + 400*b <= 127000", "20*a + 10*b <= 4270", "a >= 0", "b >= 0"]
 restricted_pso = RestrictedPSO()
-gbest, gbest_fit, gbest_fit_hist = restricted_pso.run(n_particles, n_dimensions, max_iterations, f, w, c1, c2,
-                                                      inequalities, variables)
+gbest, gbest_fit, gbest_fit_hist = restricted_pso.run(
+    n_particles, n_dimensions, max_iterations, f, w, c1, c2, inequalities, variables
+)
 
 # Se imprime la mejor solucion encontrada y también su valor optimo
 print(f"Mejor solucion: [{gbest[0]:.4f}, {gbest[1]:.4f}]")
 print(f"Valor optimo: {gbest_fit}")
 plot_vector(gbest_fit_hist, n_particles)
 
-
 # Que sucede si se reduce en 1 unidad el tiempo de acabado de la parte B.
 
-inequalities_b_reduced = ['300*a + 400*b <= 127000', '20*a + 11*b <= 4270']
-gbest, gbest_fit, gbest_fit_hist = restricted_pso.run(n_particles, n_dimensions, max_iterations, f, w, c1, c2,
-                                                      inequalities_b_reduced, variables)
+inequalities_b_reduced = [
+    "300*a + 400*b <= 127000",
+    "20*a + 11*b <= 4270",
+    "a >= 0",
+    "b >= 0",
+]
+gbest, gbest_fit, gbest_fit_hist = restricted_pso.run(
+    n_particles,
+    n_dimensions,
+    max_iterations,
+    f,
+    w,
+    c1,
+    c2,
+    inequalities_b_reduced,
+    variables,
+)
 # Se imprime la mejor solucion encontrada y también su valor optimo
 print("Aumento en 1 unidad el tiempo de acabado de la impresora 2")
 print(f"Mejor solucion: [{gbest[0]:.4f}, {gbest[1]:.4f}]")
